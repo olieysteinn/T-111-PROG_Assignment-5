ac1fdcd136dbfa7e26ec81674e54a2396014e7a9
Ólafur Eysteinn <olafurey@gmail.com>
2020-09-03 10:48:21 +0000

    Fixed

--- 5+/root.py
+++ 5+/root.py
@@ -5,9 +5,12 @@ guess = int(input("Initial guess: "))
 tolerance = float(input("What tolerance: "))
 
 # Implement the Babylonian square root algorithm
+previous_guess = 0
 count = 0
-while abs(guess - previous guess) > tolerance:
+while abs(guess - previous_guess) > tolerance:
+    previous_guess = guess
     guess = (guess + (number / guess)) / 2
+    count += 1
 
 # Don't change these lines
 print("Square root of", number, "is", round(guess, 4))
