4cc28eb760f19125c040e8a3a2a19454426dd9d1
Ólafur Eysteinn <olafurey@gmail.com>
2020-09-03 09:49:58 +0000

    Create maximum_number.py
    Created the algorithm

---
+++ maximum_number.py
@@ -0,0 +1,9 @@
+# 1. A user inputs a number
+# 2. The user inputs another number
+# 3. The numbers are compared and the bigger number is saved into a variable
+# 4. Repeat steps 2 and 3 until the user inputs a negative number
+# 5. Print out the maximum number
+
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+print("The maximum is", max_int)    # Do not change this line
