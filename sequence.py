45d837f70ffe90efcc16b4720ab9b0daa993815a
Ólafur Eysteinn <olafurey@gmail.com>
2020-09-03 10:24:55 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,8 @@
+# 1. The user inputs the length of the sequence
+# 2. Run a for loop with the sequence length and add up to the first three numbers in the sequence into variables
+# 3. Print out the three numbers
+# 4. Put the sum of the three numbers into a variable
+# 5. Set the second number as the first, the third as the second and the sum as the third
+# 6. Repeat steps 3 - 5 until the secuence length has been reached
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
