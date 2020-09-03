4cb176fe0059706ec106af3ae59aba2f2747bf17
Ólafur Eysteinn <olafurey@gmail.com>
2020-09-03 10:35:09 +0000

    First three numbers anticipated and printed

--- sequence.py
+++ sequence.py
@@ -6,3 +6,21 @@
 # 6. Repeat steps 3 - 5 until the secuence length has been reached
 
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+num_sum = 0
+
+for num in range(1, n+1):
+    if num == 1:
+        first_num = num
+        print(num)
+    elif num == 2:
+        second_num = num
+        print(num)
+    elif num == 3:
+        third_num = num
+        print(num)
+    else:
+        num_sum = first_num + second_num + third_num
+        first_num = second_num
+        second_num = third_num
+        third_num = num_sum
+        print(num_sum)
